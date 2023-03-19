4f5017172a7c821b82cfff325f657377d204df80
Verenich Tatiana <verenik91@gmail.com>
2023-03-19 15:24:06 +0300

    Добавлено решение задачи 29

--- dz_task29/Program.cs
+++ dz_task29/Program.cs
@@ -2,3 +2,33 @@
 // задаёт массив из 8 элементов и 
 // выводит их на экран.
 // 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+int[] arr = new int[8];
+
+FillArray(arr);
+PrintArray(arr);
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Random random = new Random(); 
+        array[i] = random.Next(1, 100);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    int i = 0;
+    Console.Write("[");
+    while (i < array.Length)
+    {
+        Console.Write(array[i]);
+        i++;
+        if (i < array.Length)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.Write("]");
+}
